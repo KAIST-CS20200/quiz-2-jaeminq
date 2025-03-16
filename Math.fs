05dd1e2b386235b08b3b0007554f3e1c628d0580
jaeminq <jaemin0910@kaist.ac.kr>
2025-03-16 18:21:55 +0900

    submit

--- Math.fs
+++ Math.fs
@@ -6,4 +6,10 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let rec inner_col (n: uint64) i=
+    if n=1UL then i
+    else 
+      if n%2UL=0UL then inner_col (n/2UL) (i+1)
+      else inner_col (3UL*n+1UL) (i+1)
+  inner_col (uint64 n) 0
+
